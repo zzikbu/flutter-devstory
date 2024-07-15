ce118c4384759b617b216d8d2fc6db42e41c830d
moichi <zzikbu@gmail.com>
2024-07-15 16:36:21 +0900

    feat: miso

---
+++ miso_starbucks/android/app/src/main/kotlin/com/example/miso_starbucks/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.miso_starbucks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ miso_starbucks/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:miso_starbucks/miso.dart';
+import 'package:miso_starbucks/starbucks.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AppListPage(),
+    );
+  }
+}
+
+class AppListPage extends StatelessWidget {
+  const AppListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            /// Miso
+            ListTile(
+              title: Text("1. Miso", style: TextStyle(fontSize: 24)),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => Miso()),
+              ),
+            ),
+
+            /// Starbucks
+            ListTile(
+              title: Text("2. StarBucks", style: TextStyle(fontSize: 24)),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => Starbucks()),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
