0407abb18ee91b522dc3a5ac2aa0585974bde8fb
moichi <zzikbu@gmail.com>
2024-09-13 23:06:51 +0900

    feat: 커스텀 테마 구현

---
+++ house_of_tomorrow/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/service/theme_service.dart';
+import 'package:house_of_tomorrow/src/view/shopping/shoppring_view.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ThemeService(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: context.themeService.themeData,
+      home: ShoppingView(),
+    );
+  }
+}
