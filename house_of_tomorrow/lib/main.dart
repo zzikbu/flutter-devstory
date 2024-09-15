29ccffd3d64ce72ef4c9a1ebf8ef3062fee65d58
moichi <zzikbu@gmail.com>
2024-09-15 22:33:04 +0900

    feat: 상품 화면 구현

--- house_of_tomorrow/lib/main.dart
+++ house_of_tomorrow/lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:house_of_tomorrow/src/service/lang_service.dart';
 import 'package:house_of_tomorrow/src/service/theme_service.dart';
-import 'package:house_of_tomorrow/src/view/shopping/shoppring_view.dart';
+import 'package:house_of_tomorrow/src/view/shopping/shopping_view.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
+import 'package:house_of_tomorrow/util/route_path.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -38,7 +39,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       locale: context.watch<LangService>().locale,
       theme: context.themeService.themeData,
-      home: const ShoppingView(),
+      initialRoute: RoutePath.shopping,
+      onGenerateRoute: RoutePath.onGenerateRoute,
     );
   }
 }
---
+++ house_of_tomorrow/lib/src/view/product/product_view.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/src/view/product/widget/product_bottom_sheet.dart';
+import 'package:house_of_tomorrow/src/view/product/widget/product_color_preview.dart';
+import 'package:house_of_tomorrow/src/view/product/widget/product_desc.dart';
+import 'package:house_of_tomorrow/theme/component/color_picker.dart';
+import 'package:house_of_tomorrow/theme/component/pop_button.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
+
+class ProductView extends StatefulWidget {
+  const ProductView({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  State<ProductView> createState() => _ProductViewState();
+}
+
+class _ProductViewState extends State<ProductView> {
+  /// 선택한 수량
+  int count = 1;
+
+  /// 선택한 색상
+  int colorIndex = 0;
+
+  /// 수량 업데이트 이벤트 함수
+  void onCountChanged(int newCount) {
+    setState(() {
+      count = newCount;
+    });
+  }
+
+  /// 색상 업데이트 이벤트 함수
+  void onColorIndexChanged(int newColorIndex) {
+    setState(() {
+      colorIndex = newColorIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.current.product),
+        leading: const PopButton(),
+        titleSpacing: 0,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Wrap(
+                runSpacing: 32,
+                alignment: WrapAlignment.center,
+                children: [
+                  /// ProductColorPreview
+                  ProductColorPreview(
+                    colorIndex: colorIndex,
+                    product: widget.product,
+                  ),
+
+                  /// ColorPicker
+                  ColorPicker(
+                    colorIndex: colorIndex,
+                    colorList: widget.product.productColorList.map((e) {
+                      return e.color;
+                    }).toList(),
+                    onColorSelected: onColorIndexChanged,
+                  ),
+
+                  /// ProductDesc
+                  ProductDesc(product: widget.product),
+                ],
+              ),
+            ),
+          ),
+
+          /// ProductBottomSheet
+          ProductBottomSheet(
+            count: count,
+            product: widget.product,
+            onCountChanged: onCountChanged,
+            onAddToCartPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
